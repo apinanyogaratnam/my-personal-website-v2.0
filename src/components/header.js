f200c56624e544142609a16036e64abab5359ddb
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-23 23:03:50 -0400

    resized icons and scaled properly

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,19 @@
 import PropTypes from 'prop-types'
-import { FaGithubSquare, FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa'
-import { MdEmail } from 'react-icons/md'
+import { FaGithubSquare, FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin, FaMailBulk } from 'react-icons/fa'
 
 
 const Header = ({ title, onAdd, showAdd }) => {
     return (
-        <header className="header-icon">
+        <header className="header">
             <h1>{title}</h1>
-                <div className="header-icon">
-                    <FaLinkedin />
-                    <FaGithubSquare />
-                    <FaFacebookSquare />
-                    <FaInstagramSquare />
-                    <FaTwitterSquare />
-                    <MdEmail />
-                </div>
+            <div>
+                <FaLinkedin className="header-icon" />
+                <FaGithubSquare className="header-icon" />
+                <FaFacebookSquare className="header-icon" />
+                <FaInstagramSquare className="header-icon" />
+                <FaTwitterSquare className="header-icon" />
+                <FaMailBulk className="header-icon" />
+            </div>
         </header>
     )
 }
