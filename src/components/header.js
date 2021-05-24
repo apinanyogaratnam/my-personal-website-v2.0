00064f576fed0177f7f6dec3c2958878d7442a0c
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-23 22:00:19 -0400

    installed react-icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
-// import Button form './Button'
+
 
 const Header = ({ title, onAdd, showAdd }) => {
     return (
-        
+
         <header className="header">
             <h1>{title}</h1>
         </header>
