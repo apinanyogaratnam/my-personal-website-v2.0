7b56e39ce8c5ea4b796b8abc6833fabbac85dcc7
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-24 22:02:57 -0400

    added cv container with rounded edges and icon

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { AiFillFolderOpen } from 'react-icons/ai'
+import { IconButton } from '@material-ui/core';
 
 const Resume = () => {
     return (
         <div className="resume-container">
-            <h2>My Resume</h2>
+            <h2>CV</h2>
+            <AiFillFolderOpen size="5em"/>
         </div>
     )
 }
