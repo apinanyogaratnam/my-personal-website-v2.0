043428e12f94d46787bd60a0eeed9f90e84e3ad4
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-03 00:24:40 -0400

    added small paragraph about me with header

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -3,7 +3,10 @@ import React from 'react'
 const AboutMe = () => {
     return (
         <div>
-            
+            <h1>About Me</h1>
+            <p>
+            My name is Apinan Yogaratnam with a Bachelors degree in Science. My field of study is Mathematics and Statistics.
+            </p>
         </div>
     )
 }
