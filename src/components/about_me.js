b6dd1ed1c9c92928a0e9600a37c86d8aa176aaea
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-18 21:22:26 -0400

    updated about me para

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
         <div>
             <h1>About Me</h1>
             <p>
-            My name is Apinan Yogaratnam with a Bachelors degree in Science. My field of study is Mathematics and Statistics.
+            My name is Apinan Yogaratnam currently persuing a Bachelors degree in Science. My field of study is Mathematics and Statistics.
             </p>
         </div>
     )
