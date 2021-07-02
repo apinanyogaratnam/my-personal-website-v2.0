bd7ddae362433bb529466e7edbdd16ef08d563fd
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-01 21:48:31 -0400

    added typing animation and added head and body shot image from award ceremony to website about me section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SideProjects from './components/side_projects'
 import About from './components/about'
 import AboutMe from './components/about_me'
 import React from 'react';
+import Typical from 'react-typical'
 
 
 function App() {
@@ -13,6 +14,19 @@ function App() {
     <div className="App">
         {/* <Header /> */}
         <img className="circular-image" src="/images/circular_loops.jpg" alt="Background Image of a Circular Loops" />
+        <h1>Hi, I'm Apinan Yogarantam</h1>
+        <p>I'm a{' '}
+          
+          <Typical 
+          loop={Infinity} 
+          wrapper="b" 
+          steps={[
+            'Software Developer ', 1000,
+            'Student at the University of Toronto', 1000,
+            ''
+          ]}/>
+
+        </p>
         <Resume />
         <SideProjects />
         <br />
