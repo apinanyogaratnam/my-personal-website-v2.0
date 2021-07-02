e8481e6c3fcda0dce44cae2d86609a1e72393ec9
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-02 00:03:36 -0400

    Updates

---
+++ precache-manifest.640f2298ecab28ac3d9783c55ab5da87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f6b690217340ffe6ca",
+    "url": "/my-personal-website-v2.0/static/css/main.d1a23b94.chunk.css"
+  },
+  {
+    "revision": "d7f6b690217340ffe6ca",
+    "url": "/my-personal-website-v2.0/static/js/main.d82925a8.chunk.js"
+  },
+  {
+    "revision": "1a1a31d3e42b73090076",
+    "url": "/my-personal-website-v2.0/static/js/runtime~main.8a54a102.js"
+  },
+  {
+    "revision": "3cb8f38d345d9c4c663d",
+    "url": "/my-personal-website-v2.0/static/css/2.3e5ce304.chunk.css"
+  },
+  {
+    "revision": "3cb8f38d345d9c4c663d",
+    "url": "/my-personal-website-v2.0/static/js/2.f3727f23.chunk.js"
+  },
+  {
+    "revision": "9c093a58bd47bb7f5206",
+    "url": "/my-personal-website-v2.0/static/js/3.246faef4.chunk.js"
+  },
+  {
+    "revision": "3408a326ba7ef2d735b49bcce2b64907",
+    "url": "/my-personal-website-v2.0/index.html"
+  }
+];
