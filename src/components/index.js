e7153b199d3e79dc7cfce25e24fe81f970dba73a
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-05 20:53:30 -0400

    fixed various warnings

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
 export { default as CarouselContainer } from './carousel_container.js';
 export { default as AboutMe } from './about_me.js';
-export { default as About } from './about.js';
-export { default as Header } from './header.js';
-export { default as Resume } from './resume.js';
 export { default as MainPage } from './main_page.js';
