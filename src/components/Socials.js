b8da0786a3fddac0cea99b825d8513d318418ccb
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-14 23:15:42 -0400

    added icons base visuals without links or colours

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { FaGithubSquare } from 'react-icons/fa'
+import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+import { IoLogoInstagram } from 'react-icons/io';
+import { FiTwitter, FiFacebook, FiLinkedin } from 'react-icons/fi';
+import { RiSnapchatLine, RiStackOverflowLine } from 'react-icons/ri';
+import { GrGlobe } from 'react-icons/gr';
+import { HiOutlineMail } from 'react-icons/hi';
+import '../css/custom.css';
+// create seperate file for icon imports
 
 const Socials = () => {
+    // organize sequence by most important to least important (work to personal)
     return (
-        <div>
-            <FaGithubSquare size="2em"/>
+        <div className="socials-container">
+            <IoLogoInstagram size="2em"/>
+            <AiFillGithub size="2em"/>
+            <FiTwitter size="2em"/>
+            <FiFacebook size="2em"/>
+            <GrGlobe size="2em"/>
+            <RiSnapchatLine size="2em"/>
+            <FiLinkedin size="2em"/>
+            <RiStackOverflowLine size="2em"/>
+            <HiOutlineMail size="2em"/>
         </div>
     )
 }
