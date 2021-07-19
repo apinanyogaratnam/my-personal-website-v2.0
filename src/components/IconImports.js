e2934b6d1fbf0abdca6694eeaabb86da00970260
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-18 20:43:33 -0400

    updated package-lock, imported icons from seperate file, reorganized socials code and order

---
+++ src/components/IconImports.js
@@ -0,0 +1,7 @@
+import { AiFillGithub } from 'react-icons/ai';
+import { IoLogoInstagram } from 'react-icons/io';
+import { FiTwitter, FiFacebook, FiLinkedin } from 'react-icons/fi';
+import { RiSnapchatLine, RiStackOverflowLine } from 'react-icons/ri';
+import { HiOutlineMail, HiOutlineGlobe } from 'react-icons/hi';
+export { AiFillGithub, IoLogoInstagram, FiFacebook, FiTwitter, FiLinkedin, 
+         RiSnapchatLine, RiStackOverflowLine, HiOutlineGlobe, HiOutlineMail }
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import { AiFillGithub } from 'react-icons/ai';
-import { IoLogoInstagram } from 'react-icons/io';
-import { FiTwitter, FiFacebook, FiLinkedin } from 'react-icons/fi';
-import { RiSnapchatLine, RiStackOverflowLine } from 'react-icons/ri';
-import { HiOutlineMail, HiOutlineGlobe } from 'react-icons/hi';
+import { AiFillGithub, IoLogoInstagram, FiFacebook, FiTwitter, FiLinkedin, 
+         RiSnapchatLine, RiStackOverflowLine, HiOutlineGlobe, HiOutlineMail 
+        } from './IconImports'
 import '../css/custom.css';
 // create seperate file for icon imports
 
 const Socials = () => {
-    // organize sequence by most important to least important (work to personal)
+    // front line social links
+    const instagram_link = "https://www.instagram.com/apinanyogaratnam/";
+    const github_link = "https://github.com/apinanyogaratnam";
+    const twitter_link = "https://twitter.com/apiyogaratnam";
+    const facebook_link = "https://www.facebook.com/apinan.yogaratnam.7";
+    const website_v1_link = "https://apinanyogaratnam.github.io/my-personal-website-v1.0";
+    const snapchat_link = "https://www.snapchat.com/add/apinan.y";
+    const linkedin_link = "https://www.linkedin.com/in/apinan-yogaratnam/";
+    const stackoverflow_link = "https://stackoverflow.com/users/13776723/apinan-yogaratnam";
+    const email = "apinanapinan@icloud.com";
+
+    // second line social links
+    const twitch_link = "https://www.twitch.tv/stewietheangel";
+    const spotify_link = "https://open.spotify.com/user/apinanapinan";
+    const steam_link = "https://steamcommunity.com/profiles/76561199123617568";
+    const reddit_link = "https://www.reddit.com/user/stewietheangel";
+    const youtube_link = "https://www.youtube.com/channel/UChKmTOaRJmU1VJQlDJoskeQ";
+    const discord_id = "stewietheangel#6223"
+
     return (
         <div className="socials-container">
-            <IoLogoInstagram size="2em"/>
-            <AiFillGithub size="2em"/>
-            <FiTwitter size="2em"/>
-            <FiFacebook size="2em"/>
-            <HiOutlineGlobe size="2em"/>
-            <RiSnapchatLine size="2em"/>
-            <FiLinkedin size="2em"/>
-            <RiStackOverflowLine size="2em"/>
-            <HiOutlineMail size="2em"/>
+            <a onClick={()=> window.open(github_link, "_blank")}>
+                <AiFillGithub color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(linkedin_link, "_blank")}>
+                <FiLinkedin color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(website_v1_link, "_blank")}>
+                <HiOutlineGlobe color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(email, "_blank")}>
+                <HiOutlineMail color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(instagram_link, "_blank")}>
+                <IoLogoInstagram color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(twitter_link, "_blank")}>
+                <FiTwitter color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(facebook_link, "_blank")}>
+                <FiFacebook color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(snapchat_link, "_blank")}>
+                <RiSnapchatLine color="white" size="2em"/>
+            </a>
+            <a onClick={()=> window.open(stackoverflow_link, "_blank")}>
+                <RiStackOverflowLine color="white" size="2em"/>
+            </a>
         </div>
     )
 }
