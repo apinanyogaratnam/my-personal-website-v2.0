0e2e226ad73cfa0d7ebd840706b4fd05ad9489c2
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-28 09:54:02 -0400

    current progress of project section

--- src/components/side_projects_section.js
+++ src/components/side_projects_section.js
@@ -6,6 +6,9 @@ const SideProjectsSection = () => {
             <h1>Side Projects</h1>
             <div className="project-container">
                 <h4>Project 1</h4>
+                <div className="image-container">
+                    <img></img>
+                </div>
             </div>
         </div>
     );
