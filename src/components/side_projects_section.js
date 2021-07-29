541202d5e39a1018d9d743ad0a4c425de089ec7a
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-28 20:07:03 -0400

    added borders for image and caption container css

--- src/components/side_projects_section.js
+++ src/components/side_projects_section.js
@@ -1,15 +1,43 @@
 import React from 'react';
 
 const SideProjectsSection = () => {
+    const side_projects_json_data = [
+        {
+            "title": "Stews Bot",
+            "subtitle": "Discord Bot with ML capabilities",
+            // "image_directory"
+            "caption": "Synthesized a working discord bot in python for the school’s community computer science discord server. Leveraged the use of OpenAI’s API and TensorFlow packages to create/train models and allow users to use machine learning services to ask questions and receive answers in a relatively quick amount of time."
+        }
+    ]
+
     return (
         <div className="side-projects-container">
             <h1>Side Projects</h1>
             <div className="project-container">
                 <h4>Project 1</h4>
+                <h5 className="project-subtitle">subtitle</h5>
                 <div className="image-container">
                     <img></img>
                 </div>
+                <div className="project-caption-container">
+                    <p>Things to say about the project</p>
+                </div>
             </div>
+            {side_projects_json_data.map(project => {
+                return (
+                    <div key={"id"} className="project-container">
+                        <h4>{project.title}</h4>
+                        <h5 className="project-subtitle">{project.subtitle}</h5>
+                        <h4>{project.caption}</h4>
+                        <div className="image-container">
+                            <img></img>
+                        </div>
+                        <div className="project-caption-container">
+
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
