dc93f5aad6d15fa8bba7ae17dabf2672eb8ab0d4
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-31 12:20:12 -0400

    added sample data and rendering data in class app

---
+++ src/components/data/data.js
@@ -0,0 +1,28 @@
+const data = {
+    "properties": [
+        {
+        "_id": "1",
+        "index": 0,
+        "price": 134,
+        "picture": "",
+        },
+        {
+            "_id": "2",
+            "index": 0,
+            "price": 134,
+            "picture": "",
+        },
+        {
+            "_id": "3",
+            "index": 0,
+            "price": 134,
+            "picture": "",
+        },
+        {
+            "_id": "4",
+            "index": 0,
+            "price": 134,
+            "picture": "",
+        }
+    ]
+}
--- src/components/material_design_side_projects_section.js
+++ src/components/material_design_side_projects_section.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import '../css/material_design_side_projects.css'
 
-const MaterialDesignSideProjectsSection = () => {
-    return (
-        <div className="personal-projects-container">
-            <h1>Personal Projects</h1>
-        </div>
-    )
+class MaterialDesignSideProjectsSection extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            properties: data.properties,
+            property: data.properties[0]
+        }
+    }
+    
+    nextProperty = () => {
+        const newIndex = this.state.property.index + 1;
+        this.setState({
+            property: data.properties[newIndex]
+        })
+    }
+
+    prevProperty = () => {
+        const newIndex = this.state.property.index-1;
+        this.setState({
+            property: data.properties[newIndex]
+        })
+    }
+
+    render () {
+        const {properties, property} = this.state;
+
+        return (
+            <div className="personal-projects-container">
+                <button
+                    onClick={() => this.nextProperty()}
+                    disabled={property.index === data.properties.length-1}
+                >Next</button>
+                <button
+                    onClick={() => this.prevProperty()}
+                    disabled={property.index === 0}
+                >Prev</button>
+            </div>
+        );
+    }
 }
 
 export default MaterialDesignSideProjectsSection
